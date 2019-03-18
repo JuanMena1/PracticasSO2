206f7ef861678c89eaab4d4ad7c2a40877f5fbbd
JuanMena1 <Juan.Mena1@alu.uclm.es>
2019-03-18 18:28:19 +0100

    Primer commit

---
+++ Practica2/SSOOIIGLE.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+#include <queue>
+
+class HiloBusqueda{
+    private:
+        int id_hilo;
+        int comienzo_hilo;
+        int final_hilo;
+        queue <string> informacion_busqueda;
+};
+
+list <HiloBusqueda> lista_hilos;
+
+
+void crearHilos(){
+    for(unsigned i=0; i<num_hilos;i++){
+
+    }
+}
+
+void abrirFicheros(){
+    std::ifstream.open();
+}
