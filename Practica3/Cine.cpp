d7407c4a0eb6363a83523cfc9afa1bca5dbb62c1
JuanMena1 <Juan.Mena1@alu.uclm.es>
2019-05-09 13:40:53 +0200

    Versión 2.0 Comentarios casi completados

--- Practica3/Cine.cpp
+++ Practica3/Cine.cpp
@@ -28,7 +28,7 @@
  *										controlar los Puntos de Venta
  *
  * 07/0502019	Juan Mena	5			Añadidos turnos para las colas y 
- *										manejador de interrupción Crtl + C
+ *										manejador de interrupción Ctrl + C
  *
  * *******************************************************************/
 
@@ -47,91 +47,96 @@
 #include <signal.h>
 #include <unistd.h>
 
-
+/*Estructura con la información del Punto de Venta*/
 struct InfoPuntoVenta {
 	int id;
 	int cantidad_palomitas;
 	int cantidad_bebidas;
-	int cantidad_maxima;
+	int cantidad_reponer;
 };
 
+/*Estructura para realizar los pagos*/
 struct SolicitudPago {
 	int num_cliente;
 	int tipo_pago;
 };
 
-//Clase cliente con comprar tickets, comprar palomitas
+/*Variables Globales*/
+int 								num_hilos 			= 		5;		/*Número de clientes que van a entrar al cine al principio*/
+int 								asientos_libres 	= 		35;		/*Número de asientos libres de la sala*/
+int 								turno_tickets 		= 		0;		/*Turno para la cola de los tickets*/
+int 								turno_palomitas 	= 		0;		/*Turno para la cola de las palomitas y bebidas*/
+bool 								asientos_suficientes 		{true};	/*Variable para saber si quedan asientos suficientes para asignar*/
 
-int num_hilos = 10;
-int asientos_sala = 30;
-bool asientos_suficientes {true};
-std::mutex sem_tickets;
-std::mutex sem_taquilla;
-std::mutex sem_pagar;
-std::mutex sem_palomitas;
-std::mutex sem_puntoventa;
-std::mutex sem_reponedor;
-std::mutex s_manager_tickets;
-std::mutex s_manager_palomitas;
+std::mutex 							sem_tickets;						/*Semáforo para la cola de los tickets*/
+std::mutex 							sem_taquilla;						/*Semáforo para la taquilla*/
+std::mutex 							sem_pagar;							/*Semáforo para el sistema de pago*/
+std::mutex 							sem_palomitas;						/*Semáforo para la cola de las palomitas y bebidas*/
+std::mutex 							sem_puntoventa;						/*Semáforo para los puntos de venta*/
+std::mutex 							sem_reponedor;						/*Semáforo para el reponedor*/
+std::mutex 							s_manager_tickets;					/*Semáforo para los turnos de la cola de los tickets*/
+std::mutex 							s_manager_palomitas;				/*Semáforo para los turnos de la cola de las palomitas y bebidas*/
 
-std::condition_variable cv_puntos_venta;
-std::condition_variable cv_tickets;
-std::condition_variable cv_palomitas;
+std::condition_variable 			cv_puntos_venta;					/*Variable de condición para los accesos a los puntos de venta*/
+std::condition_variable 			cv_tickets;							/*Variable de condición para los turnos de la cola de los tickets*/
+std::condition_variable 			cv_palomitas;						/*Variable de condición para los turnos de la cola de las palomitas y bebidas*/
 
-std::vector <Cliente> vector_clientes;
-std::vector <std::thread> sala;
+std::vector <Cliente> 				vector_clientes;					/*Vector para almacenar las instancias de la clase Cliente*/
+std::vector <std::thread> 			sala_cine;							/*Vector para simular los clientes dentro de la sala*/
+std::vector <std::thread> 			clientes_no_aceptados;				/*Vector para simular los clientes que se marchan por no poder darles los tickets que solicitan*/
 
-std::queue <std::thread> cola_hilos_tickets;
-std::queue <std::thread> cola_hilos_palomitas;
-std::queue <PeticionTicket> cola_peticiones_taquilla;
-std::queue <PeticionComidaBebida> cola_peticiones_palomitas;
-std::queue <InfoPuntoVenta*> cola_peticiones_reponer;
-std::queue <SolicitudPago> cola_pagos;
+std::queue 	<std::thread> 			cola_hilos_tickets;					/*Cola para pedir los tickets*/
+std::queue 	<std::thread> 			cola_hilos_palomitas;				/*Cola para pedir las palomitas y bebidas*/
+std::queue 	<PeticionTicket> 		cola_peticiones_taquilla;			/*Cola para realizar las peticiones de tickets*/
+std::queue 	<PeticionComidaBebida> 	cola_peticiones_palomitas;			/*Cola para realizar las peticiones de palomitas y bebidas*/
+std::queue 	<InfoPuntoVenta*> 		cola_peticiones_reponer;			/*Cola para solicitar reponer palomitas y bebidas de un punto de venta*/
+std::queue 	<SolicitudPago> 		cola_pagos;							/*Cola para realizar los pagos*/
 
-int turno_tickets = 0;
-int turno_palomitas = 0;
-
+/*Se encarga de recargar las palomitas y bebidas de los 3 puntos de venta*/
 void reponer(){
 	while(1){
 		sem_reponedor.lock();
+
 		InfoPuntoVenta *info_reponer = cola_peticiones_reponer.front();
 		cola_peticiones_reponer.pop();
+
 		std::cout <<RED<< "[REPONEDOR]\t Solicitud de reponer" <<RESET<<std::endl;
-		info_reponer->cantidad_bebidas = info_reponer->cantidad_maxima;
-		info_reponer->cantidad_palomitas = info_reponer->cantidad_maxima;
+
+		info_reponer->cantidad_bebidas = info_reponer->cantidad_reponer;
+		info_reponer->cantidad_palomitas = info_reponer->cantidad_reponer;
 		std::this_thread::sleep_for (std::chrono::milliseconds(1500));
+
 		std::cout << RED<<"[REPONEDOR]\t Repuestos " << info_reponer->cantidad_palomitas <<" palomitas y " << info_reponer->cantidad_bebidas <<" bebidas de Punto de Venta "<<info_reponer->id << RESET <<std::endl;		
 		std::this_thread::sleep_for (std::chrono::milliseconds(500));		
 		sem_puntoventa.unlock();
 	}
 }
-/*Variable de condicion*/
+/*Se encarga de realizar los pagos tanto de tickets como de comida y bebida*/
 void pagarTarjeta(){
 	while(1){
 		sem_pagar.lock();
 		SolicitudPago sp = cola_pagos.front();
 		cola_pagos.pop();
+
 		if(sp.tipo_pago == 1){
 			std::cout << BOLDYELLOW <<"[REALIZAR_PAGO]\t Hilo "<< sp.num_cliente << " paga sus tickets"<< RESET <<std::endl;
-			s_manager_tickets.unlock();
 		}
 		else{
 			std::cout << BOLDYELLOW <<"[REALIZAR_PAGO]\t Hilo "<< sp.num_cliente << " paga sus palomitas y bebidas"<< RESET <<std::endl;
-			s_manager_palomitas.unlock();
 		}
 	}
 }
-
+/*Se encarga de suministrar las palomitas y las bebidas a los clientes*/
 void PuntoVenta(InfoPuntoVenta& pv){
-	std::cout << "[PUNTO_VENTA " << pv.id << "]\t Abre Punto de venta "<< pv.id<<" con " << pv.cantidad_palomitas << " palomitas y " << pv.cantidad_bebidas << " bebidas" << std::endl;
+	std::cout << BOLDBLUE<<"[PUNTO_VENTA " << pv.id << "]\t Abre Punto de venta "<< pv.id<<" con " << pv.cantidad_palomitas << " palomitas y " << pv.cantidad_bebidas << " bebidas" <<RESET<< std::endl;
 	while(1){
 		std::unique_lock<std::mutex> lk_puntoventa(sem_puntoventa);
 		cv_puntos_venta.wait(lk_puntoventa, []{return !cola_peticiones_palomitas.empty();});
 
 		PeticionComidaBebida pcb = cola_peticiones_palomitas.front();
 		cola_peticiones_palomitas.pop();
-		//std::cout <<YELLOW << "[PUNTO_VENTA " << pv.id << "]\t Hilo " << pcb.getId() << " entra a Punto de Venta " << pv.id <<RESET<< std::endl;
-		std::this_thread::sleep_for (std::chrono::milliseconds(1000));		
+		std::this_thread::sleep_for (std::chrono::milliseconds(1000));	
+
 		if(pv.cantidad_bebidas - pcb.getNumBebidas() <0 || pv.cantidad_palomitas - pcb.getNumPalomitas()<0){
 			cola_peticiones_reponer.push(&pv);
 
@@ -151,17 +156,21 @@ void PuntoVenta(InfoPuntoVenta& pv){
 		sem_palomitas.unlock();
 	}
 }
-
+/*Se encarga de dar a los clientes los tickets que desean si hay hueco suficiente en la sala*/
 void servicioTaquilla(){
-	std::cout <<"[TAQUILLA]\t Abre la taquilla"<< std::endl;
+	std::cout << YELLOW <<"\n[TAQUILLA]\t Abre la taquilla"<<RESET<< std::endl;
 	while(1){
 		sem_taquilla.lock();
+
 		PeticionTicket p = cola_peticiones_taquilla.front();
 		cola_peticiones_taquilla.pop();
 		std::this_thread::sleep_for (std::chrono::milliseconds(250));		
-		if((asientos_sala - p.getNumTickets()) >= 0) {
-			asientos_sala -= p.getNumTickets();
+
+		if((asientos_libres - p.getNumTickets()) >= 0) {
+			asientos_libres -= p.getNumTickets();
 			std::cout << YELLOW << "[TAQUILLA]\t Hilo " <<p.getId()<< " ha comprado " << p.getNumTickets()<<" tickets"<< RESET <<std::endl;
+			/*En caso de que llegue un cliente que quiera justo los asientos que
+			quedan libres cuando ha llegado antes otro que quería más*/
 			if(!asientos_suficientes)
 				asientos_suficientes = true;
 			
@@ -172,25 +181,34 @@ void servicioTaquilla(){
 		sem_tickets.unlock();
 	}
 }
+/*Es el comportamiento que tiene el cliente cuando llega al cine*/
 void Cliente::comportamientoCliente(){
 	Cliente::comprarTickets(this->id);
+
 	if(asientos_suficientes){
 		cola_hilos_palomitas.push(std::move(cola_hilos_tickets.front()));
 		cola_hilos_tickets.pop();
+
+		s_manager_tickets.unlock();
+
 		Cliente::comprarPalomitas(this->id);
 		std::this_thread::sleep_for (std::chrono::milliseconds(500));
-		std::cout << BOLDMAGENTA << "[GENERAL]\t Hilo " << this->id << " entra a la sala a ver una peli" << RESET <<std::endl;
+
+		s_manager_palomitas.unlock();
+
+		std::cout << BOLDMAGENTA << "[GENERAL]\t Hilo " << this->id << " entra a la sala a ver DEADPOOL" << RESET <<std::endl;
 	}else{
+		clientes_no_aceptados.push_back(std::move(cola_hilos_tickets.front()));
+		cola_hilos_tickets.pop();
 		std::cout << BOLDMAGENTA << "[GENERAL]\t Hilo " << this->id << " se va del cine" << RESET <<std::endl;
 	}
 }
-
+/*Conjunto de acciones que realiza el cliente cuando va a solicitar los tickets*/
 void Cliente::comprarTickets(int const id){
 	std::cout << "[C_TICKETS]\t Hilo " << this->id << " llega a la cola de los tickets" << std::endl;
 	std::unique_lock<std::mutex> lk_tickets(sem_tickets);
 
 	cv_tickets.wait(lk_tickets, [id]{return(turno_tickets==id);});
-	std::cout <<BOLDWHITE <<"[C_TICKETS]\t ** Turno para tickets " << this->id << " **"<< RESET<<std::endl;
 
 	std::this_thread::sleep_for (std::chrono::milliseconds(1000));
 	std::cout << CYAN << "[C_TICKETS]\t Hilo " << this->id << " entra a taquilla para comprar " << this->num_tickets << " tickets" << RESET<<std::endl;
@@ -207,12 +225,12 @@ void Cliente::comprarTickets(int const id){
 		s_manager_tickets.unlock();
 	}
 }
-
+/*Conjunto de acciones que realiza el cliente cuando va a solicitar las palomitas y la bebida*/
 void Cliente::comprarPalomitas(int const id){
 	std::cout  << GREEN << "[C_PALOMITAS]\t Hilo " << this->id << " llega a la cola de las palomitas" << RESET<<std::endl;
 	std::unique_lock<std::mutex> lk_palomitas(sem_palomitas);
+
 	cv_palomitas.wait(lk_palomitas, [id]{return(turno_palomitas==id);});
-	std::cout <<BOLDWHITE <<"[C_PALOMITAS]\t -- Turno para palomitas " << this->id << " --"<< RESET<<std::endl;
 
 	std::this_thread::sleep_for (std::chrono::milliseconds(1000));
 	std::cout << "[C_PALOMITAS]\t Hilo " << this->id << " va a comprar " << this->num_palomitas << " palomitas y " <<this->num_bebidas << " bebidas" << std::endl;
@@ -224,82 +242,90 @@ void Cliente::comprarPalomitas(int const id){
 	sem_palomitas.lock();
 	sem_pagar.unlock();
 }
-
+/*Manejador de la señal de interrupción Ctrl+C*/
 void manejador(int sig){
     signal(sig, manejador);
-    std::cout << BOLDRED << "\n[MANEJADOR]\t Señal Ctrl+C recibida. Sacabó el cine" << RESET << std::endl;
+    std::cout << BOLDRED << "\n[MANEJADOR]\t Señal Ctrl+C recibida." << RESET << std::endl;
     kill(getpid(), SIGKILL);
     exit(EXIT_SUCCESS);
 }
 
+/*Método principal donde se crean los clientes, los hilos y donde se generan los turnos
+tanto para la cola de los tickets como para la cola de las palomitas y la bebida*/
 int main(int argc, char *argv[]) {
 	int i;
 
 	if(signal(SIGINT, manejador) == SIG_ERR) {
-    	fprintf(stderr, "[MANAGER]\t ERROR, fallo en la captura de la señal Ctrl+C\n");
+    	std::cout << BOLDRED <<"\n[GENERAL]\t ERROR, fallo en la captura de la señal Ctrl+C\n" << RESET << std::endl;
     	exit(EXIT_FAILURE);
     }
 
-	sem_taquilla.lock();
-	sem_pagar.lock();
-	sem_reponedor.lock();
-	s_manager_tickets.lock();
+	sem_taquilla.		lock();
+	sem_pagar.			lock();
+	sem_reponedor.		lock();
+	s_manager_tickets.	lock();
 	s_manager_palomitas.lock();
 
 	std::thread Taquilla(servicioTaquilla);
 	std::this_thread::sleep_for (std::chrono::milliseconds(100));
 	std::thread Pagar(pagarTarjeta);
 
-	InfoPuntoVenta pv1={1,10,10,10};
+	/*id, num_palomitas, num_bebida, cantidad_reponer*/
+	InfoPuntoVenta pv1={1,	10,	10,	10};
+	InfoPuntoVenta pv2={2,	8,	8,	8};
+	InfoPuntoVenta pv3={3,	7,	7,	7};
+
 	std::thread PuntoVenta1(PuntoVenta, std::ref(pv1));
 	std::this_thread::sleep_for (std::chrono::milliseconds(100));
 
-	InfoPuntoVenta pv2={2,8,8,8};
 	std::thread PuntoVenta2(PuntoVenta, std::ref(pv2));
 	std::this_thread::sleep_for (std::chrono::milliseconds(100));
 
-	InfoPuntoVenta pv3={3,7,7,7};
 	std::thread PuntoVenta3(PuntoVenta, std::ref(pv3));
+	std::this_thread::sleep_for (std::chrono::milliseconds(100));
+
 	std::thread Reponedor(reponer);
 
+	/*Se crean las primeros instancias de la clase Cliente*/
 	for(i=0;i<num_hilos;i++)
 		vector_clientes.push_back(Cliente(i+1, (rand() % 5)+1, (rand() % 5)+1, (rand() % 5)+1));
 
+	/*Se crean los primeros hilos para simular la llegada de clientes*/
 	for(i=0;i<num_hilos;i++){
 		cola_hilos_tickets.push(std::thread(&Cliente::comportamientoCliente, &vector_clientes[i]));
 	 	std::this_thread::sleep_for (std::chrono::milliseconds(100));
 	}
 
-	while(asientos_sala != 0){
+	while(asientos_libres != 0){
 		if(!cola_hilos_tickets.empty()){
-			turno_tickets += 1;
+			turno_tickets++;
+			std::cout <<BOLDWHITE <<"\n[C_TICKETS]\t ** Turno " << turno_tickets << " para tickets **\n"<< RESET<<std::endl;
 			cv_tickets.notify_all();
 			s_manager_tickets.lock();
 		}
-		if(!cola_hilos_palomitas.empty() || asientos_suficientes){
-			turno_palomitas += 1;
+		if(!cola_hilos_palomitas.empty()){
+			turno_palomitas++;
+			std::cout <<BOLDWHITE <<"\n[C_PALOMITAS]\t -- Turno " << turno_palomitas << " para palomitas y bebidas --\n"<< RESET<<std::endl;
 			cv_palomitas.notify_all();
 			s_manager_palomitas.lock();
-			sala.push_back(std::move(cola_hilos_palomitas.front()));
+			sala_cine.push_back(std::move(cola_hilos_palomitas.front()));
 			cola_hilos_palomitas.pop();
 		}
-		if(!asientos_suficientes){
-			turno_palomitas+=1;
-		}
-		if(((rand()%10)+1) > 7){
+		/*Se simula la llegada nuevos clientes*/
+		if(((rand()%10)+1) > 6){
 			vector_clientes.push_back(Cliente(num_hilos+1, (rand() % 5)+1, (rand() % 5)+1, (rand() % 5)+1));
 			cola_hilos_tickets.push(std::thread(&Cliente::comportamientoCliente, &vector_clientes[num_hilos]));
 	 		std::this_thread::sleep_for (std::chrono::milliseconds(100));
 			num_hilos += 1;
 		}
+		/*En caso de que el cliente se haya ido sin entrar al cine*/
+		if(!asientos_suficientes && !cola_hilos_tickets.empty()){
+			turno_palomitas++;
+		}
 	}
 
-	//Taquilla.detach();
-	//Pagar.detach();
-	//PuntoVenta1.detach();
-	//PuntoVenta2.detach();
-	//PuntoVenta3.detach();
-	//Reponedor.detach();
+	std::this_thread::sleep_for (std::chrono::milliseconds(1000));
+	std::cout << BOLDBLUE << "\n[GENERAL]\t Todos los asientos están acupados y la película va a empezar "<< RESET <<std::endl;
 
 	Pagar.join();
 	return 0;
